422191b04d40ebc410550be4234cfef11f015e4c
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-20 20:14:31 -0300

    Modulo 1 - Criando projeto Nodejs - Hello World

--- modulo01/index.js
+++ modulo01/index.js
@@ -4,6 +4,7 @@ const server = express();
 
 // localhost:3000/teste
 server.get('/teste', (req, res) => {
+  
   //return res.send('Hello World!');
   return res.json({ message: 'Hello World!'});
 })
