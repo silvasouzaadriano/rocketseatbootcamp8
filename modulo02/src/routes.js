f96e332287c9ccc11fca25139439af94d94e56ac
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-23 20:39:15 -0300

    Modulo 2 - Criando loader de models

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User'; // just for test
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World Adriano' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Adriano Souza',
+    email: 'silva.souza.adriano@gmail.com',
+    password_hash: '1234',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
