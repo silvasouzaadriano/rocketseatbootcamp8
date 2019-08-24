3f6d9ef0b94b31dbaf17d7d722e8917ff39ce9c2
Adriano R S Souza <silva.souza.adriano@gmail.com>
2019-08-23 21:12:22 -0300

    Modulo 2 - Cadastro de usuários

---
+++ modulo02/src/app/controllers/UserController.js
@@ -0,0 +1,23 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    // Verifying the user email already exist
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    // Create the user
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User'; // just for test
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Adriano Souza',
-    email: 'silva.souza.adriano@gmail.com',
-    password_hash: '1234',
-  });
-
-  return res.json(user);
-});
+// Route for user creation
+routes.post('/users', UserController.store);
 
 export default routes;
